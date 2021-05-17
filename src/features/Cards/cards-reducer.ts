5bc4a710fb85e09e6aa6c239fcb1ad0d212b572f
antonymss <demeshkevichanton@gmail.com>
2021-05-17 10:31:32 +0300

    refact

--- src/features/Cards/cards-reducer.ts
+++ src/features/Cards/cards-reducer.ts
@@ -1,17 +1,23 @@
 import {Dispatch} from "redux";
 import {RequestStatusType} from "../Login/auth-reducer";
-import {GetSortedCardsType, SortDirections} from "../../api/api";
+import {CardDataType, cardsAPI, GetSortedCardsType, NewCardDataType, SortDirections} from "../../api/api";
+import {AppRootStateType} from "../../app/store";
+import {ThunkDispatch} from "redux-thunk";
 
 const initialState = {
     requestStatus: 'idle' as RequestStatusType, //изначально статус запроса - "неактивный"
     error: '',
+    cards: [] as Array<CardDataType>,
+    packUserId: "",
+    cardsTotalCount: 0,
+    page: 0,
     sortParams: {
         question: '',
         answer: '',
         sortDirection: SortDirections.Down,
         propToSortBy: undefined,
-        minGrade: '0',
-        maxGrade: '5',
+        minGrade: 0,
+        maxGrade: 5,
         page: 1,
         pageCount: 10
     } as GetSortedCardsType
@@ -40,6 +46,15 @@ export const cardsReducer = (state = initialState, action: ActionsType): CardsSt
                 sortParams: {...state.sortParams, ...action.sortParams}
             }
         }
+        case 'CARDS/SET-CARDS': {
+            return {
+                ...state,
+                cards: action.cards,
+                packUserId: action.packUserId,
+                page: action.page,
+                cardsTotalCount: action.cardsTotalCount
+            }
+        }
         default:
             return state
     }
@@ -52,23 +67,75 @@ const setRequestStatusAC = (requestStatus: RequestStatusType) => ({
 } as const)
 const setErrorAC = (error: string) => ({type: 'CARDS/SET-ERROR', error} as const)
 const setSortParamsAC = (sortParams: GetSortedCardsType) => ({type: 'CARDS/SET-SORT-PARAMS', sortParams} as const)
+const setCardsAC = (cards: Array<CardDataType>, packUserId: string, page: number, cardsTotalCount: number) =>
+    ({type: 'CARDS/SET-CARDS', cards, packUserId, page, cardsTotalCount} as const)
 
 //thunk
-// export const resetPasswordTC = (password: string, resetPasswordToken: string | undefined) => (dispatch: ThunkDispatch) => {
-//     dispatch(setRequestStatusAC('loading'))
-//     authAPI.resetPassword({password, resetPasswordToken})
-//         .then(res => {
-//             dispatch(setInfoAC(res.data.info))
-//             dispatch(setRequestStatusAC('success'))
-//         })
-//         .catch(e => {
-//             const error = e.response
-//                 ? e.response.data.error
-//                 : (e.message + ', more details in the console')
-//             dispatch(setErrorAC(error))
-//             dispatch(setRequestStatusAC('failed'))
-//         })
-// }
+export const getCardsTC = (packId: string, params: GetSortedCardsType = {}) => (dispatch: ThunkCustommDispatch, getState: () => AppRootStateType) => {
+    if (params) dispatch(setSortParamsAC(params))
+    const sortParams = getState().cards.sortParams
+    dispatch(setRequestStatusAC('loading'))
+    cardsAPI.getCards(packId, sortParams)
+        .then(res => {
+            dispatch(setCardsAC(res.data.cards, res.data.packUserId, res.data.page, res.data.cardsTotalCount))
+            dispatch(setRequestStatusAC('success'))
+        })
+        .catch(e => {
+            const error = e.response
+                ? e.response.data.error
+                : (e.message + ', more details in the console')
+            dispatch(setErrorAC(error))
+            dispatch(setRequestStatusAC('failed'))
+        })
+}
+
+export const addCardTC = (packId: string, params?: GetSortedCardsType) => (dispatch: ThunkDispatch<AppRootStateType, void, ActionsType>) => {
+    dispatch(setRequestStatusAC('loading'))
+    cardsAPI.addCard(packId, params)
+        .then(() => {
+            dispatch(getCardsTC(packId, params))
+            dispatch(setRequestStatusAC('success'))
+        })
+        .catch(e => {
+            const error = e.response
+                ? e.response.data.error
+                : (e.message + ', more details in the console')
+            dispatch(setErrorAC(error))
+            dispatch(setRequestStatusAC('failed'))
+        })
+}
+
+export const deleteCardTC = (packId: string, cardId: string) => (dispatch: ThunkDispatch<AppRootStateType, void, ActionsType>) => {
+    dispatch(setRequestStatusAC('loading'))
+    cardsAPI.deleteCard(cardId)
+        .then(() => {
+            dispatch(getCardsTC(packId))
+            dispatch(setRequestStatusAC('success'))
+        })
+        .catch(e => {
+            const error = e.response
+                ? e.response.data.error
+                : (e.message + ', more details in the console')
+            dispatch(setErrorAC(error))
+            dispatch(setRequestStatusAC('failed'))
+        })
+}
+
+export const updateCardTC = (packId: string, cardId: string, params: NewCardDataType = {}, comments?: string) => (dispatch: ThunkDispatch<AppRootStateType, void, ActionsType>) => {
+    dispatch(setRequestStatusAC('loading'))
+    cardsAPI.updateCard(cardId, params, comments)
+        .then(() => {
+            dispatch(getCardsTC(packId))
+            dispatch(setRequestStatusAC('success'))
+        })
+        .catch(e => {
+            const error = e.response
+                ? e.response.data.error
+                : (e.message + ', more details in the console')
+            dispatch(setErrorAC(error))
+            dispatch(setRequestStatusAC('failed'))
+        })
+}
 
 //types
 export type CardsStateType = typeof initialState
@@ -77,6 +144,7 @@ export type ActionsType =
     | ReturnType<typeof setRequestStatusAC>
     | ReturnType<typeof setErrorAC>
     | ReturnType<typeof setSortParamsAC>
+    | ReturnType<typeof setCardsAC>
 
 // тип диспатча:
-type ThunkDispatch = Dispatch<ActionsType>
+type ThunkCustommDispatch = Dispatch<ActionsType>
