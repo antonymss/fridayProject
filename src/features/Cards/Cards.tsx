f9ce9453d8e558635950ef643722ad99d0d595b8
antonymss <demeshkevichanton@gmail.com>
2021-05-17 10:31:12 +0300

    refact

--- src/features/Cards/Cards.tsx
+++ src/features/Cards/Cards.tsx
@@ -1,45 +1,62 @@
-import React, {KeyboardEvent, useCallback, useEffect} from "react";
+import React, {KeyboardEvent, useCallback, useEffect, useState} from "react";
 import style from "./Cards.module.css";
-import {Redirect} from "react-router-dom";
+import {Redirect, useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../app/store";
 import {SortButtons} from "../../common/SortButtons/SortButtons";
-import {getAuthUserDataTC} from "../Login/auth-reducer";
-import {GetSortedCardsType, SortDirections} from "../../api/api";
+import {CardDataType, GetSortedCardsType, SortDirections} from "../../api/api";
 import {DoubleRange} from "../../common/DoubleRange/DoubleRange";
+import {addCardTC, CardsStateType, getCardsTC} from "./cards-reducer";
+import {Paginator} from "../Paginator/Paginator";
+import {PATH} from "../../app/App";
+import {Card} from "./Card/Card";
+
 
 export const Cards = () => {
     const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
-    // const {packId} = useParams<{ packId?: string }>()    //читаем id колоды из URL
+    const authUserId = useSelector<AppRootStateType, string>(state => state.auth._id)
+    const cards = useSelector<AppRootStateType, Array<CardDataType>>(state => state.cards.cards)
+    const packUserId = useSelector<AppRootStateType, string>(state => state.cards.packUserId)
+    const {packId} = useParams<{ packId?: any }>()    //читаем id колоды из URL
     const error = useSelector<AppRootStateType, string>(state => state.cards.error)
     const {minGrade, maxGrade} = useSelector<AppRootStateType, GetSortedCardsType>(state => state.cards.sortParams)
+    const {cardsTotalCount, page} = useSelector<AppRootStateType, CardsStateType>(state => state.cards)
     const dispatch = useDispatch()
 
+    const [answer, setAnswer] = useState<string>("")
+    const [question, setQuestion] = useState<string>("")
+
     useEffect(() => {
-        // if (isLoggedIn && packId) dispatch(getCardsTC(packId))   //запрашиваем карточки, если залогинен и есть packId
-        if (!isLoggedIn) dispatch(getAuthUserDataTC())
+        if (isLoggedIn && packId) dispatch(getCardsTC(packId))   //запрашиваем карточки, если залогинен и есть packId
     }, [])
 
     const onSortByGrade = useCallback((sortDirection: SortDirections) => {
-        // dispatch(getCardsTC({sortDirection, propToSortBy: "grade"}))
-    }, [dispatch])
+        dispatch(getCardsTC(packId, {sortDirection, propToSortBy: "grade"}))
+    }, [packId, dispatch])
 
-    const onGradeRangeChange = useCallback(([minValue, maxValue]: Array<string | undefined>) => {
-        // dispatch(getCardsTC({minGrade: minValue, maxGrade: maxValue}))
-    }, [dispatch])
+    const onGradeRangeChange = useCallback(([minValue, maxValue]: Array<number | undefined>) => {
+        dispatch(getCardsTC(packId, {minGrade: minValue, maxGrade: maxValue}))
+    }, [packId, dispatch])
 
     const onSearchByQuestion = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
-
+            dispatch(getCardsTC(packId, {question: question}))
         }
     }
     const onSearchByAnswer = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
-
+            dispatch(getCardsTC(packId, {answer: answer}))
         }
     }
+    const onAddBtnClick = (packId: string) => {
+        dispatch(addCardTC(packId))
+    }
+    const paginatorPage = useCallback((page: number, pageCount: number | undefined) => {
+        dispatch(getCardsTC(packId, {page, pageCount}))
+    }, [packId, dispatch])
 
-    if (!isLoggedIn) return <Redirect to={'/login'}/>
+    if (!isLoggedIn) return <Redirect to={PATH.LOGIN}/>
+    if (isLoggedIn && !packId) return <Redirect to={PATH.PACKS}/>
 
     return (
         <div className={style.cards}>
@@ -47,52 +64,48 @@ export const Cards = () => {
             <div className={style.filter}>
                 {/*фильтр карточек по вопросу*/}
                 <label>Search cards by question: <input placeholder={'Press Enter to search'}
-                                                        onKeyPress={onSearchByQuestion}/></label>
+                                                        onKeyPress={onSearchByQuestion}
+                                                        value={question}
+                                                        onChange={e => setQuestion(e.currentTarget.value)}/></label>
                 {/*фильтр карточек по ответу*/}
                 <label>Search cards by answer: <input placeholder={'Press Enter to search'}
-                                                      onKeyPress={onSearchByAnswer}/></label>
+                                                      onKeyPress={onSearchByAnswer}
+                                                      value={answer}
+                                                      onChange={e => setAnswer(e.currentTarget.value)}/></label>
                 {/*двойной range для сортировки по оценкам (grade)*/}
-                Search cards by grade:
-                <DoubleRange minValue={minGrade} maxValue={maxGrade} onValuesChange={onGradeRangeChange}
-                             maxRangeLimit={'5'}/>
+                <div style={{display: "flex"}}>Search cards by grade:
+                    <DoubleRange minValue={minGrade} maxValue={maxGrade} onValuesChange={onGradeRangeChange}
+                                 maxRangeLimit={5}/></div>
             </div>
             {error && <div style={{color: 'red', margin: '0 auto'}}>{error}</div>}
             <table width="100%" cellPadding="4" className={style.table}>
-                <tr style={{outline: 'medium solid'}}>
+                <thead style={{outline: 'medium solid'}}>
+                <tr>
                     <th>Question</th>
                     <th>Answer</th>
                     <th>
                         <div className={style.cellWithButtons}>Grade<SortButtons onClick={onSortByGrade}/></div>
                     </th>
-                    <th>Last Updated</th>
-                    <th>URL</th>
+                    <th>Last Update</th>
+                    <th>Pack ID</th>
                     <th>
-                        <button>Add</button>
+                        <button onClick={() => {
+                            onAddBtnClick(packId)
+                        }} disabled={packUserId !== authUserId}>Add
+                        </button>
                     </th>
                 </tr>
+                </thead>
+                <tbody>
                 {/*мапим карточки, чтобы они появились в таблице*/}
-                {/*{cards.map(c => <tr key={c._id}>*/}
-                {/*    <td>{c.question}</td>*/}
-                {/*    <td>{c.answer}</td>*/}
-                {/*    <td>{c.grade}</td>*/}
-                {/*    <td>{c.updated}</td>*/}
-                {/*    <td>string</td>*/}
-                {/*    <td>*/}
-                {/*        <button>Delete</button>*/}
-                {/*        <button>Update</button>*/}
-                {/*    </td>*/}
-                {/*</tr>)}*/}
-
+                {cards.map(c => <Card key={c._id} card={c} packId={packId} authUserId={authUserId}/>)}
+                </tbody>
             </table>
             {/*Pagination*/}
             <div className={style.pagination}>
-                Pagination
-                {/*номер текущей страницы (сначала вводим, а затем сетаем значение с сервера),*/}
-                <input type="number"/>
-                {/*отмапленные кнопки для перехода на другие страницы и*/}
-                <button>кнопки для перехода на другие страницы</button>
-                {/*общее количество страниц*/}
-                <span>общее кол-во страниц</span>
+                <Paginator current={page}
+                           total={cardsTotalCount}
+                           onChange={paginatorPage}/>
             </div>
         </div>
     );
