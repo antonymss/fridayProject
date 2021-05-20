7baa72a9f84262a4d2e156e960066d0b6fa84183
antonymss <demeshkevichanton@gmail.com>
2021-05-20 12:32:28 +0300

    refact

--- src/features/Packs/Packs.tsx
+++ src/features/Packs/Packs.tsx
@@ -1,20 +1,22 @@
-import React, {ChangeEvent, KeyboardEvent, useCallback, useEffect} from "react";
+import React, {ChangeEvent, KeyboardEvent, useCallback, useEffect, useState} from "react";
 import style from "./Packs.module.css";
 import {SortButtons} from "../../common/SortButtons/SortButtons";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../app/store";
-import {getAuthUserDataTC} from "../Login/auth-reducer";
 import {Redirect} from "react-router-dom";
-import {addPackTC, getPacksTC} from "./packs-reducer";
-import {GetSortedPacksType, PackDataType, SortDirections} from "../../api/api";
+import {addPackTC, getPacksTC, PacksStateType} from "./packs-reducer";
+import {GetSortedPacksType, SortDirections} from "../../api/api";
 import {DoubleRange} from "../../common/DoubleRange/DoubleRange";
 import {Pack} from "./Pack/Pack";
+import {Paginator} from "../Paginator/Paginator";
+import {PATH} from "../../app/App";
 
 export const Packs = () => {
     const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
     const authUserId = useSelector<AppRootStateType, string>(state => state.auth._id)
-    const packs = useSelector<AppRootStateType, Array<PackDataType>>(state => state.packs.cardPacks)
     const error = useSelector<AppRootStateType, string>(state => state.packs.error)
+    const {cardPacksTotalCount, page, cardPacks} = useSelector<AppRootStateType, PacksStateType>(state => state.packs)
+
     const {
         minCardsCount,
         maxCardsCount,
@@ -22,9 +24,10 @@ export const Packs = () => {
     } = useSelector<AppRootStateType, GetSortedPacksType>(state => state.packs.sortParams)
     const dispatch = useDispatch()
 
+    const [searchByName, setSearchByName] = useState('')
+
     useEffect(() => {
         if (isLoggedIn) dispatch(getPacksTC())
-        if (!isLoggedIn) dispatch(getAuthUserDataTC())
     }, [])
 
     const onPrivatePacksSearch = (e: ChangeEvent<HTMLInputElement>) => {
@@ -37,10 +40,10 @@ export const Packs = () => {
 
     const onSearchByName = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
-
+            dispatch(getPacksTC({nameToSearch: searchByName}))
         }
     }
-    const onCardsCountChange = useCallback(([minValue, maxValue]: Array<string | undefined>) => {
+    const onCardsCountChange = useCallback(([minValue, maxValue]: Array<number | undefined>) => {
         dispatch(getPacksTC({minCardsCount: minValue, maxCardsCount: maxValue}))
     }, [dispatch])
 
@@ -56,26 +59,35 @@ export const Packs = () => {
         dispatch(addPackTC())
     }
 
-    if (!isLoggedIn) return <Redirect to={'/login'}/>
+    const paginatorPage = useCallback((page: number, pageCount: number | undefined) => {
+        dispatch(getPacksTC({page, pageCount}))
+    }, [dispatch])
+
+    if (!isLoggedIn) return <Redirect to={PATH.LOGIN}/>
 
     return (
         <div className={style.packs}>
             <h2>Packs</h2>
             <div className={style.filter}>
                 {/*поиск приватных колод*/}
-                <label><input type="checkbox" checked={!!userId} onChange={onPrivatePacksSearch}/> including my private packs</label>
+                <label><input type="checkbox" checked={!!userId} onChange={onPrivatePacksSearch}/>
+                    show my private packs</label>
                 {/*фильтр по названию колоды*/}
                 <label>Search packs by name: <input placeholder={'Press Enter to search'}
-                                                    onKeyPress={onSearchByName}/></label>
+                                                    onKeyPress={onSearchByName}
+                                                    value={searchByName}
+                                                    onChange={e => setSearchByName(e.currentTarget.value)}
+                /></label>
 
                 {/*двойной range для сортировки по кол-ву карточек в колоде*/}
-                Search packs by cards count:
-                <DoubleRange minValue={minCardsCount} maxValue={maxCardsCount} onValuesChange={onCardsCountChange}
-                             maxRangeLimit={'200'}/>
+                <div style={{display: "flex"}}>Search packs by cards count:
+                    <DoubleRange minValue={minCardsCount} maxValue={maxCardsCount} onValuesChange={onCardsCountChange}
+                                 maxRangeLimit={200}/></div>
             </div>
             {error && <div style={{color: 'red', margin: '0 auto'}}>{error}</div>}
             <table width="100%" cellPadding="4" className={style.table}>
-                <tr style={{outline: 'medium solid'}}>
+                <thead style={{outline: 'medium solid'}}>
+                <tr>
                     <th>
                         <div className={style.cellWithButtons}>Name<SortButtons onClick={onSortByName}/></div>
                     </th>
@@ -83,27 +95,24 @@ export const Packs = () => {
                         <div className={style.cellWithButtons}>Cards Count<SortButtons onClick={onSortByCardsCount}/>
                         </div>
                     </th>
-                    <th>Last Updated</th>
-                    <th>URL</th>
+                    <th>Last Update</th>
+                    <th>Created by</th>
                     <th>
                         <button onClick={onAddBtnClick}>Add</button>
                     </th>
                 </tr>
-
+                </thead>
+                <tbody>
                 {/*мапим колоды, чтобы они появились в таблице*/}
-                {packs.map(p => <Pack key={p._id} pack={p}/>)}
+                {cardPacks.map(p => <Pack key={p._id} pack={p} authUserId={authUserId}/>)}
+                </tbody>
             </table>
             {/*Pagination*/}
             <div className={style.pagination}>
-                Pagination
-                {/*номер текущей страницы (сначала вводим, а затем сетаем значение с сервера),*/}
-                <input type="number"/>
-                {/*отмапленные кнопки для перехода на другие страницы и*/}
-                <button>кнопки для перехода на другие страницы</button>
-                {/*общее количество страниц*/}
-                <span>общее кол-во страниц</span>
+                <Paginator current={page}
+                           total={cardPacksTotalCount}
+                           onChange={paginatorPage}/>
             </div>
         </div>
     );
 }
-
