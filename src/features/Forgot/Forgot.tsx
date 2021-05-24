90b0ebf18badfae195ca3e3141d5fd6509dc8fbb
antonymss <demeshkevichanton@gmail.com>
2021-05-24 13:28:19 +0300

    refact

--- src/features/Forgot/Forgot.tsx
+++ src/features/Forgot/Forgot.tsx
@@ -4,6 +4,7 @@ import {NavLink} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../app/store";
 import {InitialForgotStateType, sendEmailToResetPassTC} from "./forgot-reducer";
+import commonStyle from "../../common/styles/error.module.css";
 
 export const Forgot = () => {
     const {requestStatus, error, info} = useSelector<AppRootStateType, InitialForgotStateType>(state => state.forgot)
@@ -22,9 +23,9 @@ export const Forgot = () => {
         <div className={style.forgot}>
             Please enter your email, and we'll send you a link to reset your password.
             {requestStatus === 'loading'
-                ? <div style={{color: 'green'}}>loading...</div>
+                ? <div className={style.loading}>loading...</div>
                 : info && <i>{info}</i>}
-            {error && <div style={{color: 'red'}}>{error}</div>}
+            <div className={error && commonStyle.error}>{error}</div>
             <input type="email" value={email} onChange={onEmailInput}/>
             <button onClick={onSendBtnClick} disabled={requestStatus === 'loading'}>Send</button>
             <NavLink to="/login" activeClassName={style.active}>Log in</NavLink>
