c38c86512cd9cf4ab982d1c42bb58d2f1a59dcf3
antonymss <demeshkevichanton@gmail.com>
2021-05-24 13:28:42 +0300

    refact

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -6,6 +6,8 @@ import {AppRootStateType} from "../../app/store";
 import {useFormik} from "formik";
 import {InitialAuthStateType, loginTC} from "./auth-reducer";
 import {PATH} from "../../app/App";
+import { Form } from "antd";
+import commonStyle from "../../common/styles/error.module.css";
 
 
 export const Login = () => {
@@ -45,8 +47,16 @@ export const Login = () => {
             <form onSubmit={formik.handleSubmit}>
 
                 <div className={style.login}>
-                    {requestStatus === 'loading' && <div style={{color: 'green'}}>loading...</div>}
-                    {error && <div style={{color: 'red'}}>{error}</div>}
+                    <Form.Item className={style.notice}>
+                        <p>To log in, please get registered
+                            <NavLink to={PATH.REGISTER} activeClassName={style.active}> here,</NavLink>
+                        </p>
+                        <p>or please use common test account credentials:</p>
+                        <p>Email: nya-admin@nya.nya</p>
+                        <p>Password: 1qazxcvBG</p>
+                    </Form.Item>
+                    {requestStatus === 'loading' && <div className={style.loading}>loading...</div>}
+                    <div className={error && commonStyle.error}>{error}</div>
                     <label>Email</label>
                     <input type="email" {...formik.getFieldProps("email")}/>
                     {formik.errors.email ? <div className={style.error}>{formik.errors.email}</div> : null}
