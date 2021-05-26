5cc95e59af2f95a2ae8bb666f38c491333ad1493
antonymss <demeshkevichanton@gmail.com>
2021-05-26 12:26:26 +0300

    refact

---
+++ src/utils/get-random-card.ts
@@ -0,0 +1,14 @@
+import {CardDataType} from "../api/api";
+
+export const getRandomCard = (cards: Array<CardDataType>): CardDataType => {
+    let cardsCopy = [...cards]
+    let totalSum = cardsCopy.reduce((acc, card) => acc + (6 - card.grade), 0)
+    let random = Math.random() * totalSum
+    let i = 0
+    let sum = 0
+    while (sum < random) {
+        sum += (6 - cardsCopy[i].grade)
+        i++
+    }
+    return cardsCopy[i-1]
+}
