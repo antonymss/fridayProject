7971dc7efe027184cea130449e65975653928687
antonymss <demeshkevichanton@gmail.com>
2021-06-01 11:20:11 +0300

    refact

--- src/features/Profile/Profile.tsx
+++ src/features/Profile/Profile.tsx
@@ -1,14 +1,15 @@
-import React, {useState} from "react";
+import React, {useCallback, useState} from "react";
 import style from "./Profile.module.css";
-import {Redirect} from "react-router-dom";
+import {NavLink, Redirect} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../app/store";
 import {InitialAuthStateType, logoutTC, updateUserDataTC} from "../Login/auth-reducer";
-import {Avatar, Popover} from 'antd';
-import {UserOutlined} from '@ant-design/icons';
-import { Typography } from 'antd';
+import {Avatar, Button, Popover, Typography} from 'antd';
+import {DeleteTwoTone, EditTwoTone, PlusSquareTwoTone, CloudServerOutlined, UserOutlined} from '@ant-design/icons';
 import {PATH} from "../../app/App";
 import commonStyle from "../../common/styles/error.module.css";
+import {FileUploader, UploadedFileDataType} from "../../common/FileUploader/FileUploader";
+import {DeleteItemModal} from "../Modals/DeleteItemModal/DeleteItemModal";
 
 export const Profile = () => {
     const {
@@ -20,28 +21,24 @@ export const Profile = () => {
         requestStatus
     } = useSelector<AppRootStateType, InitialAuthStateType>(state => state.auth)
     const dispatch = useDispatch()
-    const { Paragraph } = Typography;
-    const [newAvatarUrl, setNewAvatarUrl] = useState('')
+    const {Paragraph} = Typography;
+    const [showDeleteItemModal, setShowDeleteItemModal] = useState<boolean>(false)
 
+    const onImageEditorClick = useCallback((fileData: UploadedFileDataType) => {
+        dispatch(updateUserDataTC({avatar: fileData.base64}))
+    }, [dispatch])
+    const onDeleteAvatarClick = useCallback((isToBeDeleted: boolean) => {
+        if (isToBeDeleted) {
+            dispatch(updateUserDataTC({avatar: "0"}))
+            setShowDeleteItemModal(false)
+        }
+    }, [dispatch])
+    const onNewNameSubmit = useCallback((newName: string) => {
+        dispatch(updateUserDataTC({name: newName}))
+    }, [dispatch])
     const onLogoutClick = () => {
         dispatch(logoutTC())
     }
-    const onChangeAvatarClick = () => {
-        dispatch(updateUserDataTC({avatar: newAvatarUrl}))
-    }
-    const onNewNameSubmit = (newName: string) => {
-        dispatch(updateUserDataTC({name: newName}))
-    }
-
-    const setAvatarTools = (
-        <span>
-            <label>Enter avatar URL or Base64:
-                <input onChange={e => setNewAvatarUrl(e.currentTarget.value)}
-                       value={newAvatarUrl}/>
-            </label>
-            <button onClick={onChangeAvatarClick}>Set Avatar</button>
-        </span>
-    )
 
     if (!isLoggedIn) return <Redirect to={PATH.LOGIN}/>
 
@@ -50,17 +47,42 @@ export const Profile = () => {
             Welcome!
             {requestStatus === 'loading' && <div className={style.loading}>loading...</div>}
             <div className={error && commonStyle.error}>{error}</div>
-            {avatar
-                ? <Popover content={setAvatarTools} title="Change avatar" trigger="hover">
-                    <Avatar src={avatar} size={64}/>
-                </Popover>
-                : <Popover content={setAvatarTools} title="Add avatar" trigger="hover">
-                    <Avatar size={64}
-                            icon={<UserOutlined/>}/>
-                </Popover>}
-            <Paragraph editable={{ onChange: onNewNameSubmit }}>{name}</Paragraph>
+            <div className={style.avatarContainer}>
+                {avatar && avatar !== "0" ? <>
+                        <Avatar src={avatar} size={64}/>
+                        <div className={style.avatarButtons}>
+                            <FileUploader onClick={onImageEditorClick} expectedFileType={'image'}>
+                                <EditTwoTone style={{fontSize: '16px'}}/>
+                            </FileUploader>
+                            <div>
+                                <Button onClick={() => {
+                                    setShowDeleteItemModal(true)
+                                }} icon={<DeleteTwoTone style={{fontSize: '16px'}}/>} shape="circle" ghost/>
+                            </div>
+                        </div>
+                    </>
+                    : <>
+                        <Avatar size={64} icon={<UserOutlined/>}/>
+                        <FileUploader onClick={onImageEditorClick} expectedFileType={'image'}
+                                      style={{position: "absolute", top: "30%", right: "0"}}>
+                            <PlusSquareTwoTone style={{fontSize: '16px'}}/>
+                        </FileUploader>
+                    </>}
+            </div>
+            <Paragraph editable={{onChange: onNewNameSubmit}}>{name}</Paragraph>
             <div>{email}</div>
             <button onClick={onLogoutClick} disabled={requestStatus === 'loading'}>Log out</button>
+            {/*картинка со ссылкой на Files*/}
+            <NavLink to={PATH.FILES}>
+                <Popover title="Go to Files 🙂" trigger="hover">
+                    <CloudServerOutlined style={{position: "absolute", top: "30%", right: "30%", fontSize: '52px', color: 'orange'}}/>
+                </Popover>
+            </NavLink>
+            {/*появляющаяся модалка для удаления аватара*/}
+            {showDeleteItemModal &&
+            <DeleteItemModal itemToDelete={'image'} onDeleteBtnClick={onDeleteAvatarClick}
+                             setShow={setShowDeleteItemModal}
+                             show={showDeleteItemModal}/>}
         </div>
     );
 }
